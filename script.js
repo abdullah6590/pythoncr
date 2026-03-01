3039503a1df770888f8596bbf9a7e65aedb839e3
abdullah6590 <abdullahshah2003@gmail.com>
2026-03-01 22:39:57 +0500

    lock applied

--- script.js
+++ script.js
@@ -7,8 +7,9 @@
 // Format: 'YYYY-MM-DDTHH:MM:SS'
 const UNLOCK_DATES = {
     'oop.html': new Date('2026-02-23T13:30:00').getTime(),
-    'matplotlib.html': new Date('2026-03-03T13:30:00').getTime(),
-    'pandas.html': new Date('2026-03-02T13:30:00').getTime()
+    'matplotlib.html': new Date('2026-03-04T13:30:00').getTime(),
+    'linear-algebra.html': new Date('2026-03-02T13:30:00').getTime(),
+    'pandas.html': new Date('2026-03-03T13:30:00').getTime()
 };
 
 // --- INSTANT LOCK REDIRECT (runs before ANYTHING loads) ---
